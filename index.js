ce6c6f27649983a8b13a96d13684c228c1de8e21
Antério Vieira <anteriovieira@gmail.com>
2017-03-06 18:40:33 -0300

    Added get controller prototype

--- index.js
+++ index.js
@@ -1,132 +1,109 @@
-var angular = require('angular');
-var map = angular.__NG_HOT_MAP = Object.create(null);
-var slice = Array.prototype.slice;
-var hasOwnProperty = Object.hasOwnProperty;
-var getOwnPropertyNames = Object.getOwnPropertyNames;
-var keys = Object.keys;
-var kebabCase  = require('./utils').kebabCase;
+<template>
+    <div id="code" ng-class="{ 'minimize-code': $ctrl.$storage.minimize }">
+        <div class="code-button">
+            <span ng-click="$ctrl.toggle()" class="pull-left">
+                <icon class="fa" ng-class="{ 'fa-caret-left': $ctrl.$storage.minimize, 'fa-caret-right': !$ctrl.$storage.minimize }"></icon>
+            </span>
+            <span ng-click="$ctrl.minus()"><icon fa="minus" ng-hide=" $ctrl.$storage.minimize">
+                </icon>
+            </span>
+            <span ng-click="$ctrl.plus()"><icon fa="plus" ng-hide=" $ctrl.$storage.minimize">
+                </icon>
+            </span>
+        </div>
+        <!--<pre style="font-size: {{ $ctrl.font }}em" ng-transclude></pre>-->
+        <div class="pre" style="font-size: {{ $ctrl.$storage.codeFont }}em">
+            <json-formatter json="$ctrl.value" open="1" class="json-formatter-dark"></json-formatter>
+        </div>
+    </div>
+</template>
 
-exports.init = function () {
-    
-    var v = angular.version;
-    exports.compatible = (v.major == 1 && v.minor >= 5);
+<script>
+    export default {
+        transclude: true,
+        bindings: { value: '=' },
+        controller: ['$localStorage', function ($localStorage) {
 
-    // https://docs.angularjs.org/guide/migration#migrating-from-1-4-to-1-5
-    if (!exports.compatible) {
-        console.warn(
-          '[HMR] You are using a version of ng-hot-reload-api that is ' +
-          'only compatible with AngularJs core ^1.5.0.'
-        )
-        return;
-    }
+            this.minimize = false;
 
-    var __ng__ = angular.module;
-    function __module__(){
+            this.$storage = $localStorage.$default({
+                codeFont: 0.90,
+                minimize: false
+            });
 
-        var hijacked = __ng__.apply(this, arguments);
-        
-        if (hijacked.components) return hijacked;
-
-        hijacked.component = (function (h) {
-            var component = h.component;
-
-            return function () {
-                var args = arguments;
-
-                // We map all the components that are 
-                // registered by angular
-                if (args[1].__id && map[args[1].__id]) {
-                    map[args[1].__id].name = args[0];
-                } else {
-                    map[args[1].__id] = { name: args[0] };
-                }
-
-                if(args[1].components) {
-                     h.components(args[1].components);
-                }
-
-                // Register the component
-                return component.apply(h, args);
-            }
-        })(hijacked);
-        
-        function __queued__(name, module){
-            var exist = false;
-            hijacked._invokeQueue.forEach(function(proccess){
-                if (proccess[1] === 'component' && proccess[2][0] === name) {
-                    exist = true; return;
-                };
-            })
-            return exist;
-        }
-
-        function __components__ (components) {
-
-            if(angular.isObject(components)){
-                Object.keys(components).forEach(function (name) {
-                    var options = components[name];
-
-                    if (options.components) __components__(options.components);
-
-                    if (!__queued__(name, hijacked.name)) {
-                        hijacked.component(name, options);
-                    };
-                })
+            this.minus = f => { 
+                this.$storage.codeFont -= this.$storage.codeFont * 0.10 
             }
 
-            return hijacked;
-        }
+            this.plus = f => { 
+                this.$storage.codeFont += this.$storage.codeFont * 0.10 
+            }
 
-        hijacked.components = __components__;
-        return hijacked;
+            this.toggle = () => {
+                this.$storage.minimize = !this.$storage.minimize;
+            }
+        }]
+    }
+</script>
+
+<style>
+
+    @import '~jsonformatter/dist/json-formatter.css';
+
+    .json-formatter-dark.json-formatter-row .json-formatter-row {
+        margin-left: 2em;
     }
 
-    angular.module = __module__;
-}
-
-exports.register = function(id, component)
-{
-    map[id] = {
-        component: component
+    .json-formatter-dark.json-formatter-row .json-formatter-row:hover {
+        background: rgba(2, 2, 2, 0.21);
     }
-}
 
-exports.update = function(id, component)
-{
-    var target = angular.__NG_HOT_MAP[id];
-    var app = angular.element(document);
-    var $injector = app.injector();
-
-    if ($injector && target) {
-        var $name = target.name || target.component.name;
-        var $component = $injector.get(`${$name}Directive`)[0];
-        var $compile   = $injector.get('$compile');
-        
-        if ($component) {
-            $component.template = component.template || '';
-
-            var originComp = $component.controller.prototype;
-            var targetComp = (component.controller || function () { }).prototype;
-
-            var allProps = getOwnPropertyNames(targetComp);
-            var selProps = keys(targetComp);
-
-            var finallyProps = allProps.filter(function (key) {
-                return selProps.indexOf(key) === -1 && key !== 'constructor';
-            });
-
-            selProps.forEach(function (prop) {
-                originComp[prop] = targetComp[prop];
-            });
-        
-            slice.call(app.find(kebabCase($name))).forEach(function(element){
-                var $element = angular.element(element);
-                $element.html($component.template);
-                $compile($element.contents())($element.isolateScope());
-            });
-
-            app.find('html').scope().$apply();
-            console.info(`[NGC] Hot reload ${$name} from ng-component-load`)
-        }
+    #code .pre {
+        margin: 0;
+        padding: 30px 10px;
+        display: block;
+        background: #1d1f20;
+        color: #839496;
+        font-size: .85em;
+        line-height: 1.6em;
+        text-align: left;
+        overflow: scroll;
+        height: 600px;
     }
-}
+
+    #code {
+        position: fixed;
+        top: 56px;
+        right: 3px;
+        width: 441px;
+        float: right;
+        display: block;
+        z-index: 999999;
+        margin: 0;
+        opacity: 0.7;
+    }
+
+    #code:hover {
+        opacity: 1;
+    }
+
+    .code-button {
+        display: block;
+        position: absolute;
+        padding: 5px 10px;
+        width: 100%;
+        padding: 5px 30px 5px 5px;
+    }
+
+    .code-button span {
+        padding: 5px;
+        cursor: pointer;
+        position: relative;
+        float: right;
+    }
+
+    .minimize-code {
+        width: 40px !important;
+        height: 40px !important;
+    }
+</style>
